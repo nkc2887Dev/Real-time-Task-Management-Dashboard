193e95bbfcc251a654d7f496fc19e3fa9305c5fb
Naresh Chudasama <nkc2887dev@gmail.com>
2024-09-01 10:35:38 +0530

    mongoDB connect successfully

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load:[configuration] }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const dbHost = configService.get<string>('database.DB_HOST');
+        const dbPort = configService.get<string>('database.DB_PORT');
+        const dbDatabase = configService.get<string>('database.DB_DATABASE');
+        const dbUsername = configService.get<string>('database.DB_USERNAME');
+        const dbPassword = configService.get<string>('database.DB_PASSWORD');
+        
+        let uri = `mongodb://${dbHost}:${dbPort}/${dbDatabase}`;
+        if (dbUsername && dbPassword) {
+          uri = `mongodb://${dbUsername}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabase}`;
+        }
+        return { uri };
+      },
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/config/configuration.ts
@@ -0,0 +1,15 @@
+import { config as configDotenv } from 'dotenv';
+
+configDotenv();
+
+export default () => ({
+    port:  process.env.PORT || 3000,
+    database: {
+        DB_CONNECTION: process.env.DB_CONNECTION || "mongodb",
+        DB_HOST: process.env.DB_HOST || 'localhost',
+        DB_PORT: process.env.DB_PORT || '27017',
+        DB_DATABASE: process.env.DB_DATABASE || 'TaskManagement',
+        DB_USERNAME: process.env.DB_PASSWORD || '',
+        DB_PASSWORD: process.env.DB_PASSWORD || ''        
+    }
+});
